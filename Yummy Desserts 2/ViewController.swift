f13ce8a71509ecae0d506e62cd87c16688c683bd
Farrukh Askari <farrukh.askari01@gmail.com>
2016-01-30 05:48:06 -0800

    Initial Commit

---
+++ Yummy Desserts 2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Yummy Desserts 2
+//
+//  Created by Syed Askari on 30/01/2016.
+//  Copyright © 2016 Syed Askari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Yummy Desserts 2Tests/Yummy_Desserts_2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Yummy_Desserts_2Tests.swift
+//  Yummy Desserts 2Tests
+//
+//  Created by Syed Askari on 30/01/2016.
+//  Copyright © 2016 Syed Askari. All rights reserved.
+//
+
+import XCTest
+@testable import Yummy_Desserts_2
+
+class Yummy_Desserts_2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
